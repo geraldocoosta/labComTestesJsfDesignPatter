2ed828cc8a89369ac3b9bc485a72fc671e56a46c
José Geraldo Costa Oliveira <e539740@mail.caixa>
2019-02-27 09:22:21 -0300

    Renomeando src folder

---
+++ src/test/java/br/com/ultcode/argentum/indicadores/MediaMovelSimplesTest.java
@@ -0,0 +1,23 @@
+package br.com.ultcode.argentum.indicadores;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.ultcode.argentum.modelo.SerieTemporal;
+
+class MediaMovelSimplesTest {
+
+    @Test
+    void testandoSeRetornaAMediaPonderadaComValoresCorretos() {
+
+	SerieTemporal serieTemporal = UtilGeraSeriesTemporais.retornaSerieTemporal(1, 2, 3, 4, 5, 6);
+
+	MediaMovelSimples mms = new MediaMovelSimples();
+	assertEquals(6.0 / 3, mms.calcula(serieTemporal, 2), 0.00001);
+	assertEquals(9.0 / 3, mms.calcula(serieTemporal, 3), 0.00001);
+	assertEquals(12.0 / 3, mms.calcula(serieTemporal, 4), 0.00001);
+	assertEquals(15.0 / 3, mms.calcula(serieTemporal, 5), 0.00001);
+    }
+
+}
---
+++ src/test/java/br/com/ultcode/argentum/reader/LeitorXmlTest.java
@@ -0,0 +1,44 @@
+package br.com.ultcode.argentum.reader;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.ultcode.argentum.modelo.Negociacao;
+
+class LeitorXmlTest {
+
+    @Test
+    public void carregaXMLComUmaNegociacaoApenas() {
+
+        String xmlDeTeste = "<list>\n" + 
+                            "  <negociacao>\n" +
+                            "    <preco>40.5</preco>\n" +
+                            "    <quantidade>100</quantidade>\n" + 
+                            "    <data>\n" +
+                            "      <time>1459479600000</time>\n" +
+                            "      <timezone>America/Sao_Paulo</timezone>\n" +
+                            "    </data>\n" + 
+                            "  </negociacao>\n" + 
+                            "</list>";
+
+        Negociacao negociacaoEsperada = new Negociacao(40.5, 100,
+                LocalDateTime.of(2016, 04, 01, 00, 00));
+
+        LeitorXml leitor = new LeitorXml();
+
+        InputStream xml = new ByteArrayInputStream(xmlDeTeste.getBytes());
+
+        List<Negociacao> negociacoes = leitor.carrega(xml);
+
+        assertEquals(1, negociacoes.size());
+        assertEquals(negociacaoEsperada, negociacoes.get(0));
+    }
+
+}
