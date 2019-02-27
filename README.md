b32b662c333ea0a3fa62394164f5a0f5a8e67ae6
Geraldo <geraldocoosta@gmail.com>
2019-02-27 00:26:36 -0300

    vai cavalo

--- README.md
+++
@@ -1,3 +0,0 @@
-# labComTestesJsfDesignPatter
-Repositório para projeto feito assistindo o curso na Alura 
-Para configurar, é só importar como um javaproject, padrão
---
+++ src/main/java/br/com/ultcode/argentum/ws/ClientWebServiceTest.java
@@ -0,0 +1,14 @@
+package br.com.ultcode.argentum.ws;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ClientWebServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
